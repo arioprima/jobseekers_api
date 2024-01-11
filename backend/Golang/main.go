2e1e95423d63f17b4049077bfbb0ebc5494e04df
arioprima <arioprima2@gmail.com>
2024-01-11 14:45:13 +0700

    feat: create connection to database

--- backend/Golang/main.go
+++ backend/Golang/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/arioprima/Jobseeker/tree/main/backend/Golang/initializers"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//checkConeection to database success or not
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Println("Load config error", err)
+	}
+	_, err = initializers.ConnectDB(&config)
+	if err != nil {
+		log.Println("Connect to database error", err)
+	} else {
+		log.Println("Connect to database successfully")
+	}
 }
