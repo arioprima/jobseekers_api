a76378ce7182c603e26e478a93df24bbd7db4b85
arioprima <arioprima2@gmail.com>
2024-01-11 14:18:33 +0700

    feat: create users models

---
+++ backend/Golang/models/user.go
@@ -0,0 +1,58 @@
+package models
+
+import "time"
+
+type User struct {
+	UserID            string    `json:"id"`
+	Email             string    `json:"email"`
+	Password          string    `json:"password"`
+	FirstUser         bool      `json:"first_user"`
+	IsActive          bool      `json:"is_active"`
+	IsVerified        bool      `json:"is_verified"`
+	VerificationToken string    `json:"verification_token"`
+	RoleID            string    `json:"role_id"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+}
+
+type AdminUser struct {
+	AdminID    string `json:"id"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	BirthPlace string `json:"birth_place"`
+	BirthDate  string `json:"date_of_birth"`
+	UserID     string `json:"user_id"`
+}
+
+type RegisterInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type VerifyInput struct {
+	Email     string `json:"email"`
+	TokenType string `json:"token_type"`
+	Token     string `json:"token"`
+}
+
+type LoginResponse struct {
+	Email     string `json:"email"`
+	FirstUser bool   `json:"first_user"`
+	TokenType string `json:"token_type"`
+	Token     string `json:"token"`
+}
+
+type UserAdminResponse struct {
+	UserID     string `json:"id"`
+	Email      string `json:"email"`
+	RoleID     string `json:"role_id"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	BirthPlace string `json:"birth_place"`
+	BirthDate  string `json:"date_of_birth"`
+}
