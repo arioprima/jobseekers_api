259766e7f4ff6f28fc95a598f8e9be1003d770af
Ario Prima <arioprima2@gmail.com>
2024-02-04 23:43:45 +0700

    feat: create hero section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Home from './pages/home/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import LandingPage from './pages/LandingPage/LandingPage'
 
 function App() {
   return (
     <Router basename='/jobseekers'>
     <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
---
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../../components/Hero/Hero.tsx'
+
+
+const LandingPage = () => {
+  return (
+    <Hero />
+  )
+}
+
+export default LandingPage
