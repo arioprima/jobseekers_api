ce1b4cbea14eaf2708d9751e73df28a61299bc87
arioprima <arioprima2@gmail.com>
2024-07-06 23:47:44 +0700

    fix(models): add missing field profile_image to model user

--- models/users.go
+++ models/users.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type User struct {
-	ID         string    `json:"id" validate:"uuid"`
-	BiodataId  string    `json:"biodata_id" validate:"required,uuid" gore:"column:biodata_id"`
-	Password   string    `json:"password" validate:"required"`
-	IsActive   bool      `json:"is_active,omitempty" gorm:"default:true,column:is_active"`
-	IsVerified bool      `json:"is_verified,omitempty" gorm:"default:false,column:is_verified"`
-	RoleId     string    `json:"role_id" validate:"required,uuid" gorm:"column:role_id"`
-	Summary    string    `json:"summary,omitempty"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID           string    `json:"id" validate:"uuid"`
+	BiodataId    string    `json:"biodata_id" validate:"required,uuid" gore:"column:biodata_id"`
+	Password     string    `json:"password" validate:"required"`
+	IsActive     bool      `json:"is_active,omitempty" gorm:"default:true,column:is_active"`
+	IsVerified   bool      `json:"is_verified,omitempty" gorm:"default:false,column:is_verified"`
+	ProfileImage string    `json:"profile_image,omitempty" gorm:"column:profile_image"`
+	RoleId       string    `json:"role_id" validate:"required,uuid" gorm:"column:role_id"`
+	Summary      string    `json:"summary,omitempty"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
