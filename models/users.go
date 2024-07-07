284980e73711f54183cd815ed79494a030f9b382
arioprima <arioprima2@gmail.com>
2024-07-07 19:54:49 +0700

    fix(models): fix bug gorm typing

--- models/users.go
+++ models/users.go
@@ -6,8 +6,8 @@ type User struct {
 	ID           string    `json:"id" validate:"uuid"`
 	BiodataId    string    `json:"biodata_id" validate:"required,uuid" gore:"column:biodata_id"`
 	Password     string    `json:"password" validate:"required"`
-	IsActive     bool      `json:"is_active,omitempty" gorm:"default:true,column:is_active"`
-	IsVerified   bool      `json:"is_verified,omitempty" gorm:"default:false,column:is_verified"`
+	IsActive     bool      `json:"is_active,omitempty" gorm:"default:true"`
+	IsVerified   bool      `json:"is_verified,omitempty"  gorm:"default:false"`
 	ProfileImage string    `json:"profile_image,omitempty" gorm:"column:profile_image"`
 	RoleId       string    `json:"role_id" validate:"required,uuid" gorm:"column:role_id"`
 	Summary      string    `json:"summary,omitempty"`
