4c43da5bcdd41109f99ff7a5016865a653a4199c
arioprima <arioprima2@gmail.com>
2024-07-13 21:23:28 +0700

    feat: create middleware

--- routes/auth_route.go
+++ routes/auth_route.go
@@ -2,11 +2,17 @@ package routes
 
 import (
 	handlers "github.com/arioprima/jobseekers_api/handlers/auth"
+	"github.com/arioprima/jobseekers_api/middlewares"
 	repositories "github.com/arioprima/jobseekers_api/repositories/auth"
 	services "github.com/arioprima/jobseekers_api/services/auth"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
+
+type TestMiddleware struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func SetupAuthRoutes(route *gin.Engine, db *gorm.DB) {
 	// Initialize dependencies
@@ -17,4 +23,14 @@ func SetupAuthRoutes(route *gin.Engine, db *gorm.DB) {
 	// Setup routes
 	groupRoute := route.Group("/api")
 	groupRoute.POST("/login", loginHandler.LoginHandler)
+
+	user := TestMiddleware{
+		ID:   1,
+		Name: "Test Middleware",
+	}
+
+	groupRoute.GET("/test", middlewares.AuthMiddleware(""), func(ctx *gin.Context) {
+		ctx.JSON(200, user)
+	})
+
 }
