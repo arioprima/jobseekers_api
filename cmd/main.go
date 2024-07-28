fc93a28f5434f8274cf65baae877f671fa159ede
arioprima <arioprima2@gmail.com>
2024-07-28 21:00:53 +0700

    feat: create documentation api using sweager

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/arioprima/jobseekers_api/config"
+	_ "github.com/arioprima/jobseekers_api/docs"
 	"github.com/arioprima/jobseekers_api/middlewares"
 	"github.com/arioprima/jobseekers_api/routes"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -20,6 +23,19 @@ func main() {
 	router := gin.Default()
 
 	middlewares.SetupCorsMiddleware(router)
+
+	router.GET("/swagger.yaml", func(c *gin.Context) {
+		c.File("docs/swagger.yaml")
+	})
+
+	router.GET("/job-vacancies-api/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("/swagger.yaml")))
+
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(404, gin.H{
+			"code":    "PAGE_NOT_FOUND",
+			"message": "Page not found",
+		})
+	})
 
 	routeGroup := router.Group("/job-vacancies-api")
 
